e2f86d5441d32f367d4a3124b041269a8bddb6db
Ian <ian01story@gmail.com>
2022-11-25 16:13:48 +0100

    Fixes backend

--- backend/src/seeders/20221125140433-demo-user.js
+++ backend/src/seeders/20221125140433-demo-user.js
@@ -7,10 +7,55 @@ module.exports = {
       'supermarkets',
       [
         {
-          id: 2,
+          id: 9,
+          cityCode: '590',
+          regionCode: '56',
+          centerType: 'TYPE_B',
+          opArea: 6,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id: 10,
           cityCode: '679',
           regionCode: '56',
           centerType: 'TYPE_A',
+          opArea: 45,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id: 11,
+          cityCode: '648',
+          regionCode: '34',
+          centerType: 'TYPE_A',
+          opArea: 400,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id: 12,
+          cityCode: '679',
+          regionCode: '56',
+          centerType: 'TYPE_A',
+          opArea: 2,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id: 13,
+          cityCode: '785',
+          regionCode: '56',
+          centerType: 'TYPE_B',
+          opArea: 67,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id: 14,
+          cityCode: '555',
+          regionCode: '94',
+          centerType: 'TYPE_C',
           opArea: 3,
           createdAt: new Date(),
           updatedAt: new Date(),
@@ -22,9 +67,51 @@ module.exports = {
       'products',
       [
         {
-          id: 1,
+          id: 2,
+          category: 'TYPE_A',
+          cuisine: 'Spanish',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id: 3,
           category: 'TYPE_A',
           cuisine: 'Italian',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id: 4,
+          category: 'TYPE_B',
+          cuisine: 'Italian',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id: 5,
+          category: 'TYPE_B',
+          cuisine: 'English',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id: 6,
+          category: 'TYPE_C',
+          cuisine: 'Mexican',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id: 7,
+          category: 'TYPE_C',
+          cuisine: 'Thai',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id: 8,
+          category: 'TYPE_D',
+          cuisine: 'Spanish',
           createdAt: new Date(),
           updatedAt: new Date(),
         },
@@ -34,11 +121,7 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete('products', null, {});
+    await queryInterface.bulkDelete('supermarkets', null, {});
   },
 };
